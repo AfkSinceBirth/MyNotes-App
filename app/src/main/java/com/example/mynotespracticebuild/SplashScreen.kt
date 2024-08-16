32e46d1e676cb504df1128f14a9c8b1ddffae63b
AfkSinceBirth <om.pandey.2520@gmail.com>
2024-08-16 18:13:37 +0530

    Changed the App Icon and Added a functionality to check if there is netwrok access available and only open the app is there is internet or else close the app from splash screen

--- app/src/main/java/com/example/mynotespracticebuild/SplashScreen.kt
+++ app/src/main/java/com/example/mynotespracticebuild/SplashScreen.kt
@@ -1,7 +1,10 @@
 package com.example.mynotespracticebuild
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -28,16 +31,28 @@ class SplashScreen : AppCompatActivity() {
             delay(1000)
             val auth: FirebaseAuth = Firebase.auth
             val user = auth.currentUser
-            if(user == null) {
-                val intent = Intent(this@SplashScreen, LoginActivity::class.java)
-                startActivity(intent)
+
+            if(isNetworkAvailable(this@SplashScreen)){
+                if(user == null) {
+                    val intent = Intent(this@SplashScreen, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                    startActivity(intent)
+                }
             }
             else{
-                val intent = Intent(this@SplashScreen, MainActivity::class.java)
-                startActivity(intent)
+                Toast.makeText(this@SplashScreen,"No Internet Connection",Toast.LENGTH_SHORT).show()
             }
             finish()
         }
 
+    }
+
+    fun isNetworkAvailable(context : Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo  = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 }
